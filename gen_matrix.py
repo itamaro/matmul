409a2edf595551b0e8b7a799f776617240e7aa53
Itamar Ostricher <itamarost@gmail.com>
2018-01-07 08:29:00 +0200

    More flexible matrix generation script

--- gen_matrix.py
+++ gen_matrix.py
@@ -2,8 +2,8 @@
 
 """Generate Random Matrix
 
-Writes out JSON serialized matrix, one column per line,
-each column serialized as dictionary with `id` and `features` keys.
+Writes out JSON serialized matrix, one row per line,
+each row serialized as dictionary with `id` and `features` keys.
 """
 
 import json
@@ -21,14 +21,23 @@ def gen_element(num_high, num_low):
   return elm
 
 
-def gen_matrix(num_elements, num_high, num_low):
+def gen_matrix(num_elements, first_id, num_high, num_low):
   """Generate matrix with `num_elements` columns"""
-  for elm_id in range(_NUM_ELEMENTS):
-      yield {'id': f'{elm_id:08x}', 'features': gen_element(num_high, num_low)}
+  for elm_id in range(num_elements):
+      yield {'id': f'{elm_id+first_id:08x}', 'features': gen_element(num_high, num_low)}
 
 
 if __name__ == '__main__':
-  num_high = _NUM_FEATURES // 2
-  num_low = _NUM_FEATURES - num_high
-  for col in gen_matrix(_NUM_ELEMENTS, num_high, num_low):
+  import argparse
+  parser = argparse.ArgumentParser(description='Generate a random matrix.')
+  parser.add_argument('--num-features', default=16000, type=int,
+                      help='Number of features per matrix row')
+  parser.add_argument('--num-elements', default=1000, type=int,
+                      help='Number of matrix rows to generate')
+  parser.add_argument('--first-id', default=0, type=int,
+                      help='ID to start from for generated matrix rows')
+  args = parser.parse_args()
+  num_high = args.num_features // 2
+  num_low = args.num_features - num_high
+  for col in gen_matrix(args.num_elements, args.first_id, num_high, num_low):
     print(json.dumps(col))
